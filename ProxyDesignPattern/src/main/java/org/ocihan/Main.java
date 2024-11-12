cd586b71abdcfdc6f58db14d08103a20aeb5a236
oguzhan.cihan <oguzhan.cihan@fonetbt.com>
2024-11-12 09:19:36 +0300

    test-1

--- ProxyDesignPattern/src/main/java/org/ocihan/Main.java
+++ ProxyDesignPattern/src/main/java/org/ocihan/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         OfficeInternetAccess access = new ProxyInternetAccess("Oguzhan Cihan");
         System.out.println("testtt");
+        System.out.println("testtt-1");
         access.grantInternetAccess();
         
     }
